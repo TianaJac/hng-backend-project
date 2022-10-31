05e4eb47f2292c387315a8d105394ef25bd3c869
Tiana Jac <adebimpeade39@gmail.com>
2022-10-31 17:20:31 +0100

    HNG PROJECT

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    data ={
+        "slackUsername": "tianajac",
+        "backend": true,
+        "age": 23,
+        "bio": "A backend developer (Node js)",
+    }
+    res.send(data);
+});
+
+app.listen(3000,()=>{
+    console.log('server running on port 3000')
+})
